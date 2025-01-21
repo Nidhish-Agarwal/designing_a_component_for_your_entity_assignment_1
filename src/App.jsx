82ea13d8151f7432ffe9198ce020636d13717094
Nidhish-Agarwal <nidhishagarwal14@gmail.com>
2025-01-21 09:31:47 +0530

    Completed the assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Usercard from './components/usercard.jsx';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Usercard from "./components/usercard.jsx";
+
+const userData = {
+  image: viteLogo,
+  name: "John",
+  mail: "john07@gmail.com",
+  phone: 6789504856,
+  address: "Chruch St. Bangalore",
+};
 
 function App() {
   const cards = Array.from({ length: 10 });
   const gridStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(4, 1fr)',
-    gap: '16px',
-    padding: '16px',
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    gap: "16px",
+    padding: "16px",
   };
 
   return (
     <div style={gridStyle}>
       {cards.map((_, index) => (
-        <Usercard key={index} />
+        <Usercard key={index} userData={userData} />
       ))}
     </div>
   );
--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,16 @@
 // write the component code here
+import React from "react";
+
+function usercard({ userData }) {
+  return (
+    <div>
+      <img src={userData.image} alt="Profile photo" />
+      <p>Name: {userData.name}</p>
+      <p>Mail: {userData.mail}</p>
+      <p>Phone Number: {userData.phone}</p>
+      <p>Address: {userData.address}</p>
+    </div>
+  );
+}
+
+export default usercard;
